3a6bf39b1b1276c3b56e8b025c64a1b445840b8d
LYN <2064229611@qq.com>
2023-10-02 21:01:27 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+//引入初始化的样式文件
+import '@/styles/common.scss'
+//引入懒加载指令插件并且注册
+import { lazyPlugin } from './directives'
+//引入全局组件插件
+import { componentPlugin } from './components'
+const app = createApp(App)
+app.use(createPinia())
+app.use(router)
+app.use(lazyPlugin)
+app.use(componentPlugin)
+app.mount('#app')

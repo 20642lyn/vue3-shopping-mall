45cb23e98f834ae0892a63f2a8e65481eebf677e
LYN <2064229611@qq.com>
2023-10-02 21:23:33 +0800

    登录-pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -4,12 +4,17 @@ import App from './App.vue'
 import router from './router'
 //引入初始化的样式文件
 import '@/styles/common.scss'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 //引入懒加载指令插件并且注册
 import { lazyPlugin } from './directives'
 //引入全局组件插件
 import { componentPlugin } from './components'
 const app = createApp(App)
-app.use(createPinia())
+const pinia= createPinia()
+//注册持久性插件
+pinia.use(piniaPluginPersistedstate)
+app.use(pinia)
 app.use(router)
 app.use(lazyPlugin)
 app.use(componentPlugin)

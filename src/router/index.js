a851cc2107bb9a20965e6ff3643ddd4e3263daa8
LYN <2064229611@qq.com>
2023-10-03 21:00:06 +0800

    本地-头部购物车-计算逻辑功能

--- src/router/index.js
+++ src/router/index.js
@@ -5,41 +5,46 @@ import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
+import CartList from '@/views/CartList/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path:'/',
-      component:Layout,
-      children:[
+      path: '/',
+      component: Layout,
+      children: [
         {
-          path:'',
-          component:Home
+          path: '',
+          component: Home
         },
         {
-          path:'category/:id',
-          component:Category
+          path: 'category/:id',
+          component: Category
         },
         {
-          path:'category/sub/:id',
-          component:SubCategory
+          path: 'category/sub/:id',
+          component: SubCategory
         },
         {
-          path:'detail/:id',
+          path: 'detail/:id',
           component: Detail
+        },
+        {
+          path: 'cartlist',
+          component: CartList
         }
 
       ]
     },
     {
-      path:'/login',
-      component:Login
+      path: '/login',
+      component: Login
     }
   ],
   //路由滚动行为定制
-  scrollBehavior(){
-    return{ 
-      top:0
+  scrollBehavior() {
+    return {
+      top: 0
     }
   }
 })

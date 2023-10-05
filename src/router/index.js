05ea896b33f93afbeb543072dbd94ce347038480
LYN <2064229611@qq.com>
2023-10-05 22:00:49 +0800

    会员中心-路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,9 @@ import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
 import PayBack from '@/views/Pay/PayBack.vue'
-
+import Member from '@/views/Member/index.vue'
+import UserInfo from '@/views/Member/components/UserInfo.vue'
+import UserOrder from '@/views/Member/components/UserOrder.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -38,18 +40,32 @@ const router = createRouter({
           component: CartList
         },
         {
-          path:'checkout',
-          component:Checkout
+          path: 'checkout',
+          component: Checkout
         },
         {
-          path:'pay',
-          component:Pay
+          path: 'pay',
+          component: Pay
         },
         {
-          path:'paycallback',
-          component:PayBack
+          path: 'paycallback',
+          component: PayBack
+        },
+        {
+          path: 'member',
+          component: Member,
+          children: [
+            {
+              path: 'user',
+              component: UserInfo
+            },
+            {
+              path: 'order',
+              component: UserOrder
+            }
+          ]
         }
-       
+
 
       ]
     },
